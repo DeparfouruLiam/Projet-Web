9c3abaa87a3918801db73a9939ba236a02960c63
Liam <liam.deparfouru@edu.esiee-it.fr>
2025-04-14 14:39:52 +0200

    Installed - Jkanban library

--- resources/views/pages/retros/index.blade.php
+++ resources/views/pages/retros/index.blade.php
@@ -6,4 +6,15 @@
             </span>
         </h1>
     </x-slot>
+    <link rel="stylesheet" href="{{asset('jkanban/dist/jkanban.css')}}">
+
+    <script src="{{asset("jkanban/dist/jkanban.js")}}"></script>
 </x-app-layout>
+
+<script> var kanban = new jKanban(options) </script>
+
+<main class="kanban-drag">
+    <div class="kanban-item" data-eid="item-id-1" data-username="username1">Item 1</div>
+    <div class="kanban-item" data-eid="item-id-2" data-username="username2">Item 2</div>
+</main>
+
